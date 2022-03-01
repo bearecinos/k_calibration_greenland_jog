8649f54f076def8799970a2b599e1eb0b090baed
bearecinos <beatriz.recinos@ed.ac.uk>
2022-03-01 16:07:53 +0000

    sh for vel process

--- cluster_scripts/03_Process_velocity_data/run_process_vel.sh
+++ cluster_scripts/03_Process_velocity_data/run_process_vel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Abort whenever a single step fails. Without this, bash will just continue on errors.
-set -e
 
 OUTDIR="/scratch/local/brecinos/k_calibration_greenland_jog/output_data/03_Process_velocity_data/measures/"
 mkdir -p "$OUTDIR"
 
-conda activate oggm_env
+echo $OUTDIR
+
 python ./process_vel_data.py -conf "/scratch/local/brecinos/k_calibration_greenland_jog/config.ini"
 
 # Print a final message so you can actually see it being done in the output log.
