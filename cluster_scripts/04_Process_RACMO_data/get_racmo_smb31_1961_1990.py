c107c4f6781357d7885c048894d4d2f6fafa57c7
bearecinos <beatriz.recinos@ed.ac.uk>
2022-03-02 19:47:53 +0000

    added new racmo scripts

--- cluster_scripts/04_Process_RACMO_data/get_racmo_smb31_1961_1990.py
+++ cluster_scripts/04_Process_RACMO_data/get_racmo_smb31_1961_1990.py
@@ -10,10 +10,11 @@ import pandas as pd
 from configobj import ConfigObj
 import time
 import salem
+import argparse
 
 # Imports oggm
 import oggm.cfg as cfg
-from oggm import workflow
+from oggm import workflow, utils
 from oggm import tasks
 from oggm.workflow import execute_entity_task
 
@@ -23,64 +24,82 @@ log = logging.getLogger(__name__)
 # Time
 start = time.time()
 
-MAIN_PATH = os.path.expanduser('~/k_calibration_greenland_jog/')
-sys.path.append(MAIN_PATH)
+# Parameters to pass into the python script form the command line
+parser = argparse.ArgumentParser()
+parser.add_argument("-conf", type=str, default="../../../config.ini", help="pass config file")
+parser.add_argument("-mode", type=bool, default=False, help="pass running mode")
+args = parser.parse_args()
+config_file = args.conf
+run_mode = args.mode
 
-config = ConfigObj(os.path.join(MAIN_PATH, 'config.ini'))
+config = ConfigObj(os.path.expanduser(config_file))
+MAIN_PATH = config['main_repo_path']
+input_data_path = config['input_data_folder']
+sys.path.append(MAIN_PATH)
 
 # misc and racmo modules
 from k_tools import utils_racmo as utils_racmo
+from k_tools import misc
 
 # Region Greenland
 rgi_region = '05'
+rgi_version = '61'
 
 # Initialize OGGM and set up the run parameters
 # ---------------------------------------------
-
 cfg.initialize()
-rgi_version = '61'
 
-SLURM_WORKDIR = os.environ["WORKDIR"]
-
-# Local paths (where to write output and where to download input)
-WORKING_DIR = SLURM_WORKDIR
-cfg.PATHS['working_dir'] = WORKING_DIR
+# Define working directories (either local if run_mode = true)
+# or in the cluster environment
+if run_mode:
+    cfg.PATHS['working_dir'] = utils.get_temp_dir('GP-test-run')
+else:
+    SLURM_WORKDIR = os.environ.get("OUTDIR")
+    # Local paths (where to write output and where to download input)
+    WORKING_DIR = SLURM_WORKDIR
+    cfg.PATHS['working_dir'] = WORKING_DIR
 
 # Use multiprocessing
-cfg.PARAMS['use_multiprocessing'] = True
+if run_mode:
+    cfg.PARAMS['use_multiprocessing'] = False
+else:
+    # ONLY IN THE CLUSTER!
+    cfg.PARAMS['use_multiprocessing'] = True
+    cfg.PARAMS['mp_processes'] = 16
 
-# We make the border 20 so we can use the Columbia itmix DEM
 cfg.PARAMS['border'] = 20
-# Set to True for operational runs
 cfg.PARAMS['continue_on_error'] = True
 cfg.PARAMS['min_mu_star'] = 0.0
+cfg.PARAMS['clip_mu_star'] = True
 cfg.PARAMS['inversion_fs'] = 5.7e-20
 cfg.PARAMS['use_tar_shapefiles'] = False
 cfg.PARAMS['use_intersects'] = True
 cfg.PARAMS['use_compression'] = False
 cfg.PARAMS['compress_climate_netcdf'] = False
+cfg.PARAMS['clip_tidewater_border'] = False
 
 # RACMO data path
-racmo_path = os.path.join(MAIN_PATH, config['racmo_path'])
+racmo_path = os.path.join(input_data_path, config['racmo_path'])
 
 # RGI file
-rgidf = gpd.read_file(os.path.join(MAIN_PATH, config['RGI_FILE']))
+rgidf = gpd.read_file(os.path.join(input_data_path, config['RGI_FILE']))
 rgidf.crs = salem.wgs84.srs
 
 # We use intersects
-cfg.set_intersects_db(os.path.join(MAIN_PATH, config['intercepts']))
-
+cfg.set_intersects_db(os.path.join(input_data_path, config['intercepts']))
 rgidf = rgidf.sort_values('RGIId', ascending=True)
 
-# Read Areas for the ice-cap computed in OGGM during
-# the pre-processing runs
-df_prepro_ic = pd.read_csv(os.path.join(MAIN_PATH,
-                                        config['ice_cap_prepro']))
-df_prepro_ic = df_prepro_ic.sort_values('rgi_id', ascending=True)
+if not run_mode:
+    # Read Areas for the ice-cap computed in OGGM during
+    # the pre-processing runs
+    df_prepro_ic = pd.read_csv(os.path.join(MAIN_PATH,
+                                            config['ice_cap_prepro']))
 
-# Assign an area to the ice cap from OGGM to avoid errors
-rgidf.loc[rgidf['RGIId'].str.match('RGI60-05.10315'),
-          'Area'] = df_prepro_ic.rgi_area_km2.values
+    df_prepro_ic = df_prepro_ic.sort_values('rgi_id', ascending=True)
+
+    # Assign an area to the ice cap from OGGM to avoid errors
+    rgidf.loc[rgidf['RGIId'].str.match('RGI60-05.10315'),
+              'Area'] = df_prepro_ic.rgi_area_km2.values
 
 # Run only for Lake Terminating and Marine Terminating
 glac_type = ['0']
@@ -93,19 +112,14 @@ connection = [2]
 keep_connection = [(i not in connection) for i in rgidf.Connect]
 rgidf = rgidf.iloc[keep_connection]
 
-# Exclude glaciers with prepro erros
-de = pd.read_csv(os.path.join(MAIN_PATH, config['prepro_err']))
+# Exclude glaciers with prepro-erros
+de = pd.read_csv(os.path.join(input_data_path, config['prepro_err']))
 ids = de.RGIId.values
 keep_errors = [(i not in ids) for i in rgidf.RGIId]
 rgidf = rgidf.iloc[keep_errors]
 
-# # Run a single id for testing
-# glacier = ['RGI60-05.00304', 'RGI60-05.08443']
-# keep_indexes = [(i in glacier) for i in rgidf.RGIId]
-# rgidf = rgidf.iloc[keep_indexes]
-
 # Remove glaciers that need to be model with gimp
-df_gimp = pd.read_csv(os.path.join(MAIN_PATH, config['glaciers_gimp']))
+df_gimp = pd.read_csv(os.path.join(input_data_path, config['glaciers_gimp']))
 keep_indexes_no_gimp = [(i not in df_gimp.RGIId.values) for i in rgidf.RGIId]
 keep_gimp = [(i in df_gimp.RGIId.values) for i in rgidf.RGIId]
 rgidf_gimp = rgidf.iloc[keep_gimp]
@@ -118,16 +132,23 @@ log.info('Number of glaciers with GIMP: {}'.format(len(rgidf_gimp)))
 
 # Go - initialize working directories
 # -----------------------------------
-gdirs = workflow.init_glacier_directories(rgidf)
+if run_mode:
+    keep_index_to_run = [(i in config['RGI_id_to_test']) for i in rgidf.RGIId]
+    rgidf = rgidf.iloc[keep_index_to_run]
+    log.info('Starting run for RGI reg: ' + rgi_region)
+    log.info('Number of glaciers with ArcticDEM: {}'.format(len(rgidf)))
+    gdirs = workflow.init_glacier_directories(rgidf)
+    workflow.execute_entity_task(tasks.define_glacier_region, gdirs,
+                                 source='ARCTICDEM')
+else:
+    gdirs = workflow.init_glacier_directories(rgidf)
+    workflow.execute_entity_task(tasks.define_glacier_region, gdirs,
+                                 source='ARCTICDEM')
 
-workflow.execute_entity_task(tasks.define_glacier_region, gdirs,
-                             source='ARCTICDEM')
-
-gdirs_gimp = workflow.init_glacier_directories(rgidf_gimp)
-workflow.execute_entity_task(tasks.define_glacier_region, gdirs_gimp,
-                             source='GIMP')
-
-gdirs.extend(gdirs_gimp)
+    gdirs_gimp = workflow.init_glacier_directories(rgidf_gimp)
+    workflow.execute_entity_task(tasks.define_glacier_region, gdirs_gimp,
+                                 source='GIMP')
+    gdirs.extend(gdirs_gimp)
 
 execute_entity_task(tasks.glacier_masks, gdirs)
 
@@ -193,3 +214,5 @@ dr = {'RGI_ID': ids,
 
 df_r = pd.DataFrame(data=dr)
 df_r.to_csv(cfg.PATHS['working_dir']+'racmo_data_'+time_start+'_'+time_end+'.csv')
+
+misc.reset_per_glacier_working_dir()
