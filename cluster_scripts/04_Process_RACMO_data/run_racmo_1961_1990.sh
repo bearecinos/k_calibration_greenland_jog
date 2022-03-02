f8004726b99b8d1f54bdd4edc73b9367743f2756
bearecinos <beatriz.recinos@ed.ac.uk>
2022-03-02 16:48:42 +0000

    added racmo bash script

---
+++ cluster_scripts/04_Process_RACMO_data/run_racmo_1961_1990.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Abort whenever a single step fails. Without this, bash will just continue on errors.
+
+OUTDIR="/scratch/local/brecinos/k_calibration_greenland_jog/output_data/04_Process_RACMO_data/1961_1990/"
+mkdir -p "$OUTDIR"
+
+echo $OUTDIR
+
+python ./get_racmo_smb31_1961_1990.py -conf "/scratch/local/brecinos/k_calibration_greenland_jog/config.ini"
+
+# Print a final message so you can actually see it being done in the output log.
+echo "RUN DONE"
+
