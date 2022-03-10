5be1a07fa30b4fa3facca6134ae9095119e7445a
bearecinos <beatriz.recinos@ed.ac.uk>
2022-03-10 17:15:17 +0000

    correct run gen script

--- cluster_scripts/09_Configurations/run_generic.sh
+++ cluster_scripts/09_Configurations/run_generic.sh
@@ -11,9 +11,9 @@ echo $OUTDIR_low
 echo $OUTDIR_obs
 echo $OUTDIR_up
 
-#python ${1}/${1}_lowbound.py -conf "/scratch/local/brecinos/k_calibration_greenland_jog/config.ini"
-#python ${1}/${1}_value.py -conf "/scratch/local/brecinos/k_calibration_greenland_jog/config.ini"
-#python ${1}/${1}_upbound.py -conf "/scratch/local/brecinos/k_calibration_greenland_jog/config.ini"
+python ${1}/${1}_lowbound.py -conf "/scratch/local/brecinos/k_calibration_greenland_jog/config.ini"
+python ${1}/${1}_value.py -conf "/scratch/local/brecinos/k_calibration_greenland_jog/config.ini"
+python ${1}/${1}_upbound.py -conf "/scratch/local/brecinos/k_calibration_greenland_jog/config.ini"
 
 # Print a final message so you can actually see it being done in the output log.
 echo "RUN DONE"
