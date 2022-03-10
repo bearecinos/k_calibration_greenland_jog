bc6dbc4e53eabcc0fc5554525d3940913c7ba60b
bearecinos <beatriz.recinos@ed.ac.uk>
2022-03-10 16:37:55 +0000

    configurations update

--- cluster_scripts/09_Configurations/k_itslive/k_itslive_upbound.py
+++ cluster_scripts/09_Configurations/k_itslive/k_itslive_upbound.py
@@ -10,12 +10,14 @@ import pandas as pd
 import numpy as np
 from configobj import ConfigObj
 import time
+import argparse
 
 # Imports oggm
 import oggm.cfg as cfg
 from oggm import workflow
 from oggm import tasks
 from oggm.workflow import execute_entity_task
+from oggm import utils
 from oggm.core import inversion
 
 # Module logger
@@ -24,62 +26,81 @@ log = logging.getLogger(__name__)
 # Time
 start = time.time()
 
-MAIN_PATH = os.path.expanduser('~/k_calibration_greenland_jog/')
-sys.path.append(MAIN_PATH)
+# Parameters to pass into the python script form the command line
+parser = argparse.ArgumentParser()
+parser.add_argument("-conf", type=str, default="../../../config.ini", help="pass config file")
+parser.add_argument("-mode", type=bool, default=False, help="pass running mode")
+args = parser.parse_args()
+config_file = args.conf
+run_mode = args.mode
 
-config = ConfigObj(os.path.join(MAIN_PATH, 'config.ini'))
+config = ConfigObj(os.path.expanduser(config_file))
+MAIN_PATH = config['main_repo_path']
+input_data_path = config['input_data_folder']
+sys.path.append(MAIN_PATH)
 
 from k_tools import misc
 from k_tools import utils_velocity as utils_vel
 
 # Region Greenland
 rgi_region = '05'
+rgi_version = '61'
 
 # Initialize OGGM and set up the run parameters
 # ---------------------------------------------
-
 cfg.initialize()
-rgi_version = '61'
 
-SLURM_WORKDIR = os.environ["WORKDIR"]
+# Define working directories (either local if run_mode = true)
+# or in the cluster environment
+if run_mode:
+    cfg.PATHS['working_dir'] = utils.get_temp_dir('GP-test-run')
+else:
+    SLURM_WORKDIR = os.environ.get("OUTDIR")
+    # Local paths (where to write output and where to download input)
+    WORKING_DIR = SLURM_WORKDIR
+    cfg.PATHS['working_dir'] = WORKING_DIR
 
-# Local paths (where to write output and where to download input)
-WORKING_DIR = SLURM_WORKDIR
-cfg.PATHS['working_dir'] = WORKING_DIR
+print(cfg.PATHS['working_dir'])
 
 # Use multiprocessing
-cfg.PARAMS['use_multiprocessing'] = True
+if run_mode:
+    cfg.PARAMS['use_multiprocessing'] = True
+    cfg.PARAMS['mp_processes'] = 5
+else:
+    # ONLY IN THE CLUSTER!
+    cfg.PARAMS['use_multiprocessing'] = True
+    cfg.PARAMS['mp_processes'] = 16
 
-# We make the border 20 so we can use the Columbia itmix DEM
 cfg.PARAMS['border'] = 20
-# Set to True for operational runs
 cfg.PARAMS['continue_on_error'] = True
 cfg.PARAMS['min_mu_star'] = 0.0
+cfg.PARAMS['clip_mu_star'] = True
 cfg.PARAMS['inversion_fs'] = 5.7e-20
 cfg.PARAMS['use_tar_shapefiles'] = False
 cfg.PARAMS['use_intersects'] = True
 cfg.PARAMS['use_compression'] = False
 cfg.PARAMS['compress_climate_netcdf'] = False
-
+cfg.PARAMS['clip_tidewater_border'] = False
 
 # RGI file
-rgidf = gpd.read_file(os.path.join(MAIN_PATH, config['RGI_FILE']))
+rgidf = gpd.read_file(os.path.join(input_data_path, config['RGI_FILE']))
 rgidf.crs = salem.wgs84.srs
 
 # We use intersects
-cfg.set_intersects_db(os.path.join(MAIN_PATH, config['intercepts']))
-
+cfg.set_intersects_db(os.path.join(input_data_path, config['intercepts']))
 rgidf = rgidf.sort_values('RGIId', ascending=True)
 
-# Read Areas for the ice-cap computed in OGGM during
-# the pre-processing runs
-df_prepro_ic = pd.read_csv(os.path.join(MAIN_PATH,
-                                        config['ice_cap_prepro']))
-df_prepro_ic = df_prepro_ic.sort_values('rgi_id', ascending=True)
+if not run_mode:
+    # Read Areas for the ice-cap computed in OGGM during
+    # the pre-processing runs
+    df_prepro_ic = pd.read_csv(os.path.join(MAIN_PATH,
+                                            config['ice_cap_prepro']))
 
-# Assign an area to the ice cap from OGGM to avoid errors
-rgidf.loc[rgidf['RGIId'].str.match('RGI60-05.10315'),
-          'Area'] = df_prepro_ic.rgi_area_km2.values
+    df_prepro_ic = df_prepro_ic.sort_values('rgi_id', ascending=True)
+
+    # Assign an area to the ice cap from OGGM to avoid errors
+    rgidf.loc[rgidf['RGIId'].str.match('RGI60-05.10315'),
+              'Area'] = df_prepro_ic.rgi_area_km2.values
 
 # Run only for Lake Terminating and Marine Terminating
 glac_type = ['0']
@@ -92,8 +113,8 @@ connection = [2]
 keep_connection = [(i not in connection) for i in rgidf.Connect]
 rgidf = rgidf.iloc[keep_connection]
 
-# Exclude glaciers with prepro erros
-de = pd.read_csv(os.path.join(MAIN_PATH, config['prepro_err']))
+# Exclude glaciers with prepro-erros
+de = pd.read_csv(os.path.join(input_data_path, config['prepro_err']))
 ids = de.RGIId.values
 keep_errors = [(i not in ids) for i in rgidf.RGIId]
 rgidf = rgidf.iloc[keep_errors]
@@ -116,17 +137,12 @@ keep_no_data = [(i not in ids_no_data) for i in rgidf.RGIId]
 rgidf = rgidf.iloc[keep_no_data]
 
 # Remove glaciers that need to be model with gimp
-df_gimp = pd.read_csv(os.path.join(MAIN_PATH, config['glaciers_gimp']))
+df_gimp = pd.read_csv(os.path.join(input_data_path, config['glaciers_gimp']))
 keep_indexes_no_gimp = [(i not in df_gimp.RGIId.values) for i in rgidf.RGIId]
 keep_gimp = [(i in df_gimp.RGIId.values) for i in rgidf.RGIId]
 rgidf_gimp = rgidf.iloc[keep_gimp]
 
 rgidf = rgidf.iloc[keep_indexes_no_gimp]
-
-# # Run a single id for testing
-# glacier = ['RGI60-05.12265']
-# keep_indexes = [(i in glacier) for i in rgidf.RGIId]
-# rgidf = rgidf.iloc[keep_indexes]
 
 log.info('Starting run for RGI reg: ' + rgi_region)
 log.info('Number of glaciers with ArcticDEM: {}'.format(len(rgidf)))
@@ -134,18 +150,25 @@ log.info('Number of glaciers with GIMP: {}'.format(len(rgidf_gimp)))
 
 # Go - initialize working directories
 # -----------------------------------
-gdirs = workflow.init_glacier_directories(rgidf)
+if run_mode:
+    keep_index_to_run = [(i in config['RGI_id_to_test']) for i in rgidf.RGIId]
+    rgidf = rgidf.iloc[keep_index_to_run]
+    log.info('Starting run for RGI reg: ' + rgi_region)
+    log.info('Number of glaciers with ArcticDEM: {}'.format(len(rgidf)))
+    gdirs = workflow.init_glacier_directories(rgidf)
+    workflow.execute_entity_task(tasks.define_glacier_region, gdirs,
+                                 source='ARCTICDEM')
+else:
+    gdirs = workflow.init_glacier_directories(rgidf)
+    workflow.execute_entity_task(tasks.define_glacier_region, gdirs,
+                                 source='ARCTICDEM')
 
-workflow.execute_entity_task(tasks.define_glacier_region,
-                             gdirs, source='ARCTICDEM')
+    gdirs_gimp = workflow.init_glacier_directories(rgidf_gimp)
+    workflow.execute_entity_task(tasks.define_glacier_region, gdirs_gimp,
+                                 source='GIMP')
+    gdirs.extend(gdirs_gimp)
 
-gdirs_gimp = workflow.init_glacier_directories(rgidf_gimp)
-workflow.execute_entity_task(tasks.define_glacier_region,
-                             gdirs_gimp, source='GIMP')
-
-gdirs.extend(gdirs_gimp)
-
-# Prepro tasks
+# Pre-pro tasks
 task_list = [
     tasks.glacier_masks,
     tasks.compute_centerlines,
@@ -162,9 +185,7 @@ for task in task_list:
 for gdir in gdirs:
     gdir.inversion_calving_rate = 0
 
-execute_entity_task(tasks.process_cru_data, gdirs)
-execute_entity_task(tasks.local_t_star, gdirs)
-execute_entity_task(tasks.mu_star_calibration, gdirs)
+workflow.climate_tasks(gdirs, base_url=config['climate_url'])
 
 # Inversion tasks
 execute_entity_task(tasks.prepare_for_inversion, gdirs, add_debug_var=True)
@@ -199,6 +220,9 @@ for gdir in gdirs:
 
     cfg.PARAMS['continue_on_error'] = False
     cfg.PARAMS['inversion_calving_k'] = float(k_value)
+    cfg.PARAMS['use_kcalving_for_inversion'] = True
+    cfg.PARAMS['use_kcalving_for_ru'] = True
+
     out = inversion.find_inversion_calving(gdir)
 
     inversion.compute_velocities(gdir)
---
+++ cluster_scripts/09_Configurations/run_generic.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export OUTDIR_low="/scratch/local/brecinos/k_calibration_greenland_jog/output_data/09_Configurations/${1}/${1}_lowbound"
+export OUTDIR_obs="/scratch/local/brecinos/k_calibration_greenland_jog/output_data/09_Configurations/${1}/${1}_value"
+export OUTDIR_up="/scratch/local/brecinos/k_calibration_greenland_jog/output_data/09_Configurations/${1}/${1}_upbound"
+mkdir -p "$OUTDIR_low"
+mkdir -p "$OUTDIR_obs"
+mkdir -p "$OUTDIR_up"
+
+echo $OUTDIR_low
+echo $OUTDIR_obs
+echo $OUTDIR_up
+
+python ${1}/${1}_lowbound.py -conf "/scratch/local/brecinos/k_calibration_greenland_jog/config.ini"
+python ${1}/${1}_value.py -conf "/scratch/local/brecinos/k_calibration_greenland_jog/config.ini"
+python ${1}/${1}_upbound.py -conf "/scratch/local/brecinos/k_calibration_greenland_jog/config.ini"
+
+# Print a final message so you can actually see it being done in the output log.
+echo "RUN DONE"
