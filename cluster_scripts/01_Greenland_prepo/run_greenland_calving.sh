923171649cf753d946cbe5a6f854128d19cdcf63
bearecinos <beatriz.recinos@ed.ac.uk>
2022-09-08 16:45:59 +0100

    corrected width prepro runs

--- cluster_scripts/01_Greenland_prepo/run_greenland_calving.sh
+++ cluster_scripts/01_Greenland_prepo/run_greenland_calving.sh
@@ -7,7 +7,7 @@ mkdir -p "$OUTDIR"
 
 echo $OUTDIR
 
-python ./run_calving_greenland_default.py -conf "/scratch/local/brecinos/k_calibration_greenland_jog/config.ini"
+python ./run_calving_greenland_default.py -conf "/scratch/local/brecinos/k_calibration_greenland_jog/config.ini" -correct_width True
 
 # Print a final message so you can actually see it being done in the output log.
 echo "RUN DONE"
